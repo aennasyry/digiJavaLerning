3c56732c86f896fa41492485515bf4281652ac62
alae <alae.ennasyry@gmail.com>
2024-07-12 15:27:27 +0200

    push des class ExerciceDeclarationTab et ExerciceTableauChaines (tp3) essai N2

--- src/fr/tableaux/ExerciceDeclarationTab.java
+++ src/fr/tableaux/ExerciceDeclarationTab.java
@@ -5,5 +5,8 @@ public class ExerciceDeclarationTab {
         int tab [] = {8,-7,12,1,-2,14,17,9};
         System.out.println("tab[0] = "+tab[0]);
 
+        System.out.println("la longueur du tableau tab est : "+tab.length);
+        System.out.println("tab[7] = "+tab[7]);
+
     }
 }
--- src/fr/tableaux/ExerciceTableauChaines.java
+++ src/fr/tableaux/ExerciceTableauChaines.java
@@ -1,2 +1,20 @@
-package fr.tableaux;public class ExerciceTableauChaines {
+package fr.tableaux;
+
+public class ExerciceTableauChaines {
+    public static void main(String[] args){
+
+        String tab[] =  {"Paris", "Toulouse", "pau", "Le Havre", "Tarbes"};
+
+        System.out.println("les cinq villes enregistrées sont : ");
+        for(int i = 0; i<5; i++){
+            System.out.print("\t"+tab[i]);
+        }
+
+        tab[2] = "Reims";
+
+        System.out.println("\nAprès modification, les cinq villes enregistrées sont : ");
+        for(int i = 0; i<5; i++){
+            System.out.print("\t"+tab[i]);
+        }
+    }
 }
