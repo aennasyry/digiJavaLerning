31eeb77d677da73ab10a1677863392269798915c
alae <alae.ennasyry@gmail.com>
2024-07-12 12:39:58 +0200

    tp2 réalisé, essai de push N2

--- src/fr/declaration/variable/DeclarationApp.java
+++ src/fr/declaration/variable/DeclarationApp.java
@@ -1,2 +1,22 @@
-package fr.declaration.variable;public class DeclarationApp {
+package fr.declaration.variable;
+
+public class DeclarationApp {
+    public static void main(String[] args){
+        byte a =1;
+        short b = 2;
+        int c = 1;
+        long d = 1;
+        float e = 1;
+        double f = 1;
+        char g = 'f';
+        boolean h = true;
+        String i = "niceString";
+
+        System.out.println("la variable a est égale à : "+a);
+
+        String randomString = "Voici le résultat d'un calcul :\n 1+5=6";
+        System.out.println(randomString);
+
+
+    }
 }
