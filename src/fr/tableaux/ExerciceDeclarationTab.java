754d62dfdab61ea68d11e6c777c5cf056fa7a625
alae <alae.ennasyry@gmail.com>
2024-07-12 15:30:03 +0200

    push des class ExerciceDeclarationTab (tp3) essai N4

--- src/fr/tableaux/ExerciceDeclarationTab.java
+++ src/fr/tableaux/ExerciceDeclarationTab.java
@@ -2,7 +2,7 @@ package fr.tableaux;
 
 public class ExerciceDeclarationTab {
     public static void main(String[] args){
-        int tab [] = {8,-7,12,1,-2,14,17,9};
+        int[] tab = {8,-7,12,1,-2,14,17,9};
         System.out.println("tab[0] = "+tab[0]);
 
         System.out.println("la longueur du tableau tab est : "+tab.length);
