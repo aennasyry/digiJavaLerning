5758baa2ac2c62309e27e5df3b7f805da0639572
alae <alae.ennasyry@gmail.com>
2024-07-12 15:21:33 +0200

    push des class ExerciceDeclarationTab et ExerciceTableauChaines (tp3)

---
+++ src/fr/tableaux/ExerciceDeclarationTab.java
@@ -0,0 +1,9 @@
+package fr.tableaux;
+
+public class ExerciceDeclarationTab {
+    public static void main(String[] args){
+        int tab [] = {8,-7,12,1,-2,14,17,9};
+        System.out.println("tab[0] = "+tab[0]);
+
+    }
+}
