a6af1b7ef33f9abe016aab3b247e6e3ccb9214ca
alae <alae.ennasyry@gmail.com>
2024-07-15 11:48:09 +0200

    commit et push du tp5 algorithmie (correction de la classe TestPersonne)

--- src/entites/TestPersonne.java
+++ src/entites/TestPersonne.java
@@ -7,6 +7,7 @@ public class TestPersonne {
         Personne alae = new Personne();
         alae.nom = "ennasyry";
         alae.prenom = "alae";
+        alae.adresse = new AdressePostale();
         alae.adresse.numRue = 10;
         alae.adresse.libelleRue = "haynakhil";
         alae.adresse.codePostal = 30000;
@@ -15,6 +16,7 @@ public class TestPersonne {
         Personne issa = new Personne();
         issa.nom = "ennasyry";
         issa.prenom = "issam";
+        issa.adresse = new AdressePostale();
         issa.adresse.numRue = 235;
         issa.adresse.libelleRue = "atlas";
         issa.adresse.codePostal = 30000;
